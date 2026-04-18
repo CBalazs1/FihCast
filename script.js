caf8987cd136f1991da516b80496d5a6c8055afc
Czako Balazs <czakobalazs42@gmail.com>
2026-04-18 13:37:02 +0200

    Small QOL updates

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 function devMessage() {
-    alert("This a Pre-Alpha build of the site and is only used for placeholder for now as of 2026.04.17");
+    var d = new Date();
+
+    alert("This is still a place holder." + "\n" + d);
 }
